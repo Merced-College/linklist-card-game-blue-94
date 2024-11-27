d70fe243ad3db86272886fae96daee3428ad3081
blue-94 <jtenn84@gmail.com>
2024-11-27 01:57:58 +0000

    Add Comments in Card, Change LinkedList file name

--- Card.java
+++ Card.java
@@ -9,18 +9,21 @@ public class Card {
     private String cardPic;
 
     // Default constructor
+    // Assigns default King card if no values are given
     public Card() {
         cardName = "King";
         cardValue = 10; // Corrected this from a string to an int
     }
 
     // Non-default constructor
+    // Assignts the Card object with a given name and value
     public Card(String name, int value) {
         cardName = name;
         cardValue = value;
     }
 
     // Constructor with all attributes
+    // Assigns the Card object with the given values for all variables 
     public Card(String suit, String name, int value, String pic) {
         cardSuit = suit;
         cardName = name;
@@ -30,41 +33,41 @@ public class Card {
 
     // Getter methods
     public String getCardName() {
-        return cardName;
+        return cardName; // Returns card name 
     }
 
     public int getCardValue() {
-        return cardValue;
+        return cardValue; // Returns the value of the card
     }
 
     public String getCardSuit() {
-        return cardSuit;
+        return cardSuit; // Returns the suit of the card
     }
 
     public String getCardPic() {
-        return cardPic;
+        return cardPic; // Returns the name of the card picture
     }
 
     // Setter methods
     public void setCardName(String newCardName) {
-        cardName = newCardName;
+        cardName = newCardName; // Assigns cardName with the given string
     }
 
     public void setCardValue(int newCardValue) {
-        cardValue = newCardValue;
+        cardValue = newCardValue; // Assigns cardValue with the given integer 
     }
 
     public void setCardSuit(String newCardSuit) {
-        cardSuit = newCardSuit;
+        cardSuit = newCardSuit; // Assignts cardSuite with the given string for the suit
     }
 
     public void setCardPic(String newCardPic) {
-        cardPic = newCardPic;
+        cardPic = newCardPic; // Assignts cardPic with the given string for the card name
     }
 
     // Method to get combined card values
     public String getCardValues() {
-        return cardName + " " + cardValue;
+        return cardName + " " + cardValue; // Returns a string with the name and value of the card
     }
 
     // Equals method to compare two cards
@@ -76,7 +79,8 @@ public class Card {
         if (obj == null || getClass() != obj.getClass()) {
             return false; // Null or different class
         }
-        Card otherCard = (Card) obj;
+        Card otherCard = (Card) obj; // Converts obj into a card
+        // Compares the field values with those of the obj Card object; if they are different then false is returned, otherwise true.
         return cardValue == otherCard.cardValue && 
                cardName.equals(otherCard.cardName) && 
                cardSuit.equals(otherCard.cardSuit) && 
@@ -86,6 +90,7 @@ public class Card {
     // toString method for printing the card
     @Override
     public String toString() {
+        // The values of the fields are printed out upon printing a card. 
         return cardName + " of " + cardSuit + " (Value: " + cardValue + ", Picture: " + cardPic + ")";
     }
 }
