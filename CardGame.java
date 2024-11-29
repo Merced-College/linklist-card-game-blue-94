2ddc090c24074c54d80c78928789af238808b011
blue-94 <jtenn84@gmail.com>
2024-11-29 05:33:25 +0000

    Add working guessing game method

--- CardGame.java
+++ CardGame.java
@@ -14,11 +14,62 @@ import java.util.Random; // ADDED: used for random shuffling
 public class CardGame {
 	
 	private static LinkList cardList = new LinkList();  // make list
+    // ADDED: arrays of strings for the suits and values. a random integer will be generated and will access one from each array for shuffling
+    // Also used for checking player guesses
+    public static String[] suits = {"heart", "spade", "club", "diamond"};        
+    public static String[] values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
+
+    public static void game(Card[] playerHand) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your cards:");
+        for (int i = 0; i < 10; ++i) {
+            playerHand[i] = cardList.getFirst();
+            System.out.println(playerHand[i]);
+        }
+        System.out.println("Do you want instructions? yes/no");
+        String answer = "";
+        int points = 0;
+        while(!answer.equals("yes") || !answer.equals("no")) {
+            answer = scanner.nextLine();
+            if (answer.equals("yes")) {
+                System.out.println("\n10 cards will be drawn: using the drawn ten cards, guess the suit or name for new card!");
+                System.out.println("Correct answers for name will gain 5 points and correct answers for suit will gain 1");
+                System.out.println("Please enter answers in lowercase, with numbers written as words (e.g. one, two , three)");
+                break;
+            }
+            else if (answer.equals("no")) {
+                break;
+            }
+            else {
+                System.out.println("\nPlease enter a valid answer (yes/no)");
+            }            
+        }
+
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("\nGuess the suit or name:");
+            Card drawnCard = cardList.getFirst();
+            System.out.println(drawnCard);
+            answer = scanner.nextLine();
+
+            if (answer.equals(drawnCard.getCardSuit())) {
+                System.out.println("Correct suit guessed! Card added to hard. You gained 1 point");
+                points += 1;
+            }
+            else if (answer.equals(drawnCard.getCardName())) {
+                System.out.println("Correct name guessed! You gained 5 points");
+                points += 5;
+            }
+            System.out.println("Card was " + drawnCard.getCardName() + " of " + drawnCard.getCardSuit() + "s");
+            //playerHand[playerHand.length] = drawnCard;
+        }
+        System.out.println("GAME ENDED THANK YOU");
+        System.out.println(points);
+        scanner.close();
+
+    }
 
     public static void shuffle() {
-        // ADDED: arrays of strings for the suits and values. a random integer will be generated and will access one from each array
-        String[] suits = {"heart", "spade", "club", "diamond"};
-        String[] values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
+
         // Initialize random object for shuffling
         Random randNum = new Random();
 
@@ -87,26 +138,12 @@ public class CardGame {
         // Print the loaded cards
         System.out.println("Cards loaded:");
         cardList.displayList();
-		
-		Card[] playerHand = new Card[5];
-        // Pulls five cards from the deck for the player's hand
-		for(int i = 0; i < playerHand.length; i++)
-			playerHand[i] = cardList.getFirst();
-		
-        // Prints out the cards in the player's hand
-		System.out.println("players hand");
-		for(int i = 0; i < playerHand.length; i++)
-			System.out.println(playerHand[i]);
-		
-		System.out.println();
-        // Prints out the contents of the deck
-		System.out.println("the deck");
-		cardList.displayList();
-
         shuffle();
-        System.out.println("Shuffled!!!!!!!!");
-        cardList.displayList();
-
+		
+        
+		Card[] playerHand = new Card[10];
+ 
+        game(playerHand);
 	}//end main
 
 }//end class
